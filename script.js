cf3bbfdf2482189cc381ffb6725db7848b0c9ce4
Josh Errickson <josh@errickson.net>
2024-02-23 09:14:03 -0500

    Various lints

--- script.js
+++ script.js
@@ -12,13 +12,9 @@ let mcoeffValue
 let intcoeffValue
 let xvaluesArray
 let mvaluesArray
+let newDataset
 
-function computePoints(intercept,
-                       xvals,
-                       mval,
-                       xcoeff,
-                       mcoeff,
-                       intcoeff) {
+function computePoints(intercept, xvals, mval, xcoeff, mcoeff, intcoeff) {
 
   let plotpoints = []
 
@@ -37,27 +33,27 @@ function computePoints(intercept,
 
 // Colors for plotting. If it goes beyond 20, it'll just be grey
 const colors = [
-    'rgba(255, 99, 132, 1)',     // Red
-    'rgba(54, 162, 235, 1)',      // Blue
-    'rgba(255, 206, 86, 1)',      // Yellow
-    'rgba(75, 192, 192, 1)',      // Teal
-    'rgba(153, 102, 255, 1)',     // Purple
-    'rgba(255, 159, 64, 1)',      // Orange
-    'rgba(51, 204, 51, 1)',       // Green
-    'rgba(255, 0, 255, 1)',       // Magenta
-    'rgba(0, 255, 255, 1)',       // Cyan
-    'rgba(128, 128, 128, 1)',     // Gray
-    'rgba(255, 128, 0, 1)',       // Dark Orange
-    'rgba(0, 153, 0, 1)',         // Dark Green
-    'rgba(204, 0, 204, 1)',       // Dark Magenta
-    'rgba(0, 204, 204, 1)',       // Dark Cyan
-    'rgba(102, 51, 0, 1)',        // Brown
-    'rgba(255, 102, 102, 1)',     // Light Red
-    'rgba(102, 153, 255, 1)',     // Light Blue
-    'rgba(255, 204, 102, 1)',     // Light Orange
-    'rgba(204, 255, 102, 1)',     // Light Green
-    'rgba(255, 102, 255, 1)'      // Light Magenta
-];
+  'rgba(255, 99, 132, 1)',     // Red
+  'rgba(54, 162, 235, 1)',      // Blue
+  'rgba(255, 206, 86, 1)',      // Yellow
+  'rgba(75, 192, 192, 1)',      // Teal
+  'rgba(153, 102, 255, 1)',     // Purple
+  'rgba(255, 159, 64, 1)',      // Orange
+  'rgba(51, 204, 51, 1)',       // Green
+  'rgba(255, 0, 255, 1)',       // Magenta
+  'rgba(0, 255, 255, 1)',       // Cyan
+  'rgba(128, 128, 128, 1)',     // Gray
+  'rgba(255, 128, 0, 1)',       // Dark Orange
+  'rgba(0, 153, 0, 1)',         // Dark Green
+  'rgba(204, 0, 204, 1)',       // Dark Magenta
+  'rgba(0, 204, 204, 1)',       // Dark Cyan
+  'rgba(102, 51, 0, 1)',        // Brown
+  'rgba(255, 102, 102, 1)',     // Light Red
+  'rgba(102, 153, 255, 1)',     // Light Blue
+  'rgba(255, 204, 102, 1)',     // Light Orange
+  'rgba(204, 255, 102, 1)',     // Light Green
+  'rgba(255, 102, 255, 1)'      // Light Magenta
+]
 
 const ctx = document.getElementById('myChart').getContext('2d')
 const myChart = new Chart(ctx, {
@@ -115,48 +111,53 @@ function updateChart() {
   intcoeffValue = parseFloat(intcoeffInput.value)
   intcoeffValue = !isNaN(intcoeffValue) ? intcoeffValue : 0
   let xtmp = xvaluesInput.value.split(',')
-  xvaluesArray = [...new Set(
-    xtmp
-        .map(xtmp => parseFloat(xtmp.trim()))
-        .filter(number => !isNaN(number))
-  )]
+  xvaluesArray = [
+    ...new Set(
+      xtmp
+        .map((xtmp) => parseFloat(xtmp.trim()))
+        .filter((number) => !isNaN(number)),
+    ),
+  ]
   if (xvaluesArray.length < 2) {
     myChart.data.datasets = []
     myChart.update()
-    myChart.options.plugins.legend.title.display = false;
-    myChart.options.plugins.legend.title.text = '';
+    myChart.options.plugins.legend.title.display = false
+    myChart.options.plugins.legend.title.text = ''
     return
   }
   let mtmp = mvaluesInput.value.split(',')
-  mvaluesArray = [...new Set(
-    mtmp
-        .map(mtmp => parseFloat(mtmp.trim()))
-        .filter(number => !isNaN(number))
-  )]
+  mvaluesArray = [
+    ...new Set(
+      mtmp
+        .map((mtmp) => parseFloat(mtmp.trim()))
+        .filter((number) => !isNaN(number)),
+    ),
+  ]
 
   myChart.data.datasets = []
   for (let i = 0; i < mvaluesArray.length; i++) {
     newDataset = {
       label: `${mvaluesArray[i]}`,
-      data: computePoints(interceptValue,
-                          xvaluesArray,
-                          mvaluesArray[i],
-                          xcoeffValue,
-                          mcoeffValue,
-                          intcoeffValue),
+      data: computePoints(
+        interceptValue,
+        xvaluesArray,
+        mvaluesArray[i],
+        xcoeffValue,
+        mcoeffValue,
+        intcoeffValue),
       fill: false,
       borderColor: colors[i],
       backgroundColor: colors[i],
       pointStyle: drawpointsInput.checked ? 'circle' : false,
       tension: 0.1
     }
-    myChart.data.datasets.push(newDataset);
+    myChart.data.datasets.push(newDataset)
   }
 
   myChart.data.labels = xvaluesArray
-  myChart.options.plugins.legend.position = 'right';
-  myChart.options.plugins.legend.title.text = 'Moderator';
-  myChart.options.plugins.legend.title.display = true;
+  myChart.options.plugins.legend.position = 'right'
+  myChart.options.plugins.legend.title.text = 'Moderator'
+  myChart.options.plugins.legend.title.display = true
   myChart.update()
 }
 
